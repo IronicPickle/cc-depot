566098cbe46625e1ab14216b57dd2c52a59fcea2
IronicPickle <nathanrath@live.co.uk>
2026-01-01 16:44:12 +0000

    moved program metadata to standalone json file

--- programs/door.lua
+++ programs/door.lua
@@ -1,7 +1,4 @@
---$CC-DEPOT-META
---name: Door
---description: A basic door program
---$CC-DEPOT-META
+
 
 local function start()
     print("TEST")
--- programs/dummy.lua
+++ programs/dummy.lua
@@ -1,12 +1,3 @@
---$CC-DEPOT-META
---name: Dummy
---description: A dummy program for testing
---$CC-DEPOT-META
-
---$CC-DEPOT-CONFIG
---channel=10: Channel - The broadcast channel to be used to communicate with other dummy programs
---test=Test: Test - This is a test config option
---$CC-DEPOT-CONFIG
 
 local function start()
     print("DUMMY TEST")
