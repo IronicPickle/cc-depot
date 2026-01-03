2c13e857d121aa5128346695df12db623307fd53
IronicPickle <nathanrath@live.co.uk>
2026-01-03 23:56:59 +0000

    updated runner

--- runner/runner.lua
+++ runner/runner.lua
@@ -1,4 +1,44 @@
-local utils = require("/lua/lib/utils")
+local DIR = "$DIR$"
+local REPO_API_URL = "$REPO_API_URL$"
+local GITHUB_ACCESS_TOKEN = "$GITHUB_ACCESS_TOKEN$"
+
+local utils = require(DIR.."/lib/utils")
+local Monitor = require(DIR.."/lib/peripherals/Monitor")
+
+local TERMINAL = Monitor:new(term.current())
+
+local SELECTED_PROGRAM_INDEX = 1
+
+local function fetchRepoContents(file)
+    local res = http.get(REPO_API_URL.."/contents"..file, {
+        ["Authorization"] = "Bearer "..GITHUB_ACCESS_TOKEN,
+    })
+    if res == nil then return nil end
+    local body = textutils.unserialiseJSON(res.readAll())
+    res.close()
+    return body
+end
+
+local function getFileFromRepo(file)
+    local body = fetchRepoContents(file)
+    if not body then error("Could not download file "..file) end
+    return utils.decode64(body.content)
+end
+
+local function checkRepoReachable()
+    print("- Checking repo is reachable")
+
+
+    if not http.checkURL(REPO_API_URL) then
+        error("Repo URL not whitelisted")
+    end
+
+    if getFileFromRepo("/README.md") == nil then
+        error("Repo doesn't exist")
+    end
+
+    print("  - Repo is reachable")
+end
 
 
 
@@ -14,28 +54,29 @@ local function getProgramList()
 
         local content = getFileFromRepo("/"..file.path)
 
-        if(content == nil) then
-            print("  ! Failed to file content for file: "..file.path)
+        if not content then
+            error("No content found for file: "..file.path)
+        end
+
+        if not file.name:match("%.metadata%.json$") then
             goto continue
         end
 
+        local metadata, metadataError = textutils.unserialiseJSON(content)
 
-        local metaString = content:match("%-%-$CC%-DEPOT%-META\n(.-)\n%-%-$CC%-DEPOT%-META")
-
-        if metaString == nil then metaString = "" end
-
-        local name = content:match("%-%-name: (.-)\n")
-        local description = content:match("%-%-description: (.-)\n")
-
-        if(name == nil or description == nil) then
-            print("  ! Failed to get file meta for file: "..file.path)
-            goto continue
+        if not metadata then
+            error("Could not deserialise metadata for file: "..file.path.."\n"..metadataError)
         end
 
-        table.insert(programList, {
-            name=name,
-            description=description
-        })
+        local name = file.name:match("^(.-)%.metadata%.json")
+
+        if not name then
+            error("Could not parse name for file: "..file.path)
+        end
+
+        metadata.name = name
+
+        table.insert(programList, metadata)
 
         ::continue::
     end
@@ -46,30 +87,415 @@ end
 local function printProgramList(programList)
     term.clear()
 
-    print("- Select a program to install")
-    print("  - Use arrow keys to select and Enter to select\n")
+    local headerHeight = 4
 
+    TERMINAL:fillBackground(colors.lightBlue)
+    TERMINAL:drawBox(1, 1, TERMINAL.monitor.x, headerHeight, true, colors.blue)
+
+    TERMINAL:write("CC Depot - Installation", {
+        x=0,
+        y=2,
+        align="center",
+        textColor=colors.white,
+        bgColor=colors.blue
+    })
+    TERMINAL:write("Use Arrow Keys and Enter to select a program", {
+        x=0,
+        y=3,
+        align="center",
+        textColor=colors.lightGray,
+        bgColor=colors.blue
+    })
+
+    local bodyYOffset = headerHeight + 1
 
     for i, program in ipairs(programList) do
         local isSelected = i == SELECTED_PROGRAM_INDEX
 
-        local programString = (isSelected and "> " or "  ")..program.name.." ("..program.description..")"
+        local programString = (isSelected and "> " or "  ")..program.label.." ("..program.description..")"
 
-        print(programString)
+        TERMINAL:write(programString, {
+            x=0,
+            xPadding=3,
+            y=bodyYOffset + i,
+            textColor=colors.black,
+            bgColor=colors.lightBlue
+        })
     end
-    
 end
 
-local function startProgramSelection()
+local KEY_UP = 265
+local KEY_DOWN = 264
+local KEY_ENTER = 257
+
+local function startProgramSelection(programList)
+
+    while true do
+        printProgramList(programList)
+
+        local _, key = os.pullEvent("key_up")
+
+        if key == KEY_UP then
+            if SELECTED_PROGRAM_INDEX == 1 then goto continue end
+            SELECTED_PROGRAM_INDEX = SELECTED_PROGRAM_INDEX - 1
+        elseif key == KEY_DOWN then
+            if SELECTED_PROGRAM_INDEX == #programList then goto continue end
+            SELECTED_PROGRAM_INDEX = SELECTED_PROGRAM_INDEX + 1
+        elseif key == KEY_ENTER then
+            break
+        end
+
+        ::continue::
+    end
+end
+
+
+local function printConfigOption(option, error)
+    term.clear()
+
+    local headerHeight = 4
+
+    TERMINAL:fillBackground(colors.lightBlue)
+    TERMINAL:drawBox(1, 1, TERMINAL.monitor.x, headerHeight, true, colors.blue)
+
+    TERMINAL:write("CC Depot - Installation", {
+        x=0,
+        y=2,
+        align="center",
+        textColor=colors.white,
+        bgColor=colors.blue
+    })
+    TERMINAL:write("Program Configuration", {
+        x=0,
+        y=3,
+        align="center",
+        textColor=colors.lightGray,
+        bgColor=colors.blue
+    })
+
+    local bodyYOffset = headerHeight + 2
+
+    local hasDefault = option.default ~= nil
+    
+    TERMINAL:write(option.label..(hasDefault and " [Default: "..option.default.."]" or ""), {
+        x=0,
+        xPadding=3,
+        y=bodyYOffset,
+        textColor=colors.black,
+        bgColor=colors.lightBlue,
+        progressCursor=true,
+        wrap=true
+    })
+    TERMINAL:write("| "..option.description, {
+        x=0,
+        xPadding=3,
+        textColor=colors.black,
+        bgColor=colors.lightBlue,
+        progressCursor=true,
+        wrap=true
+    })
+    TERMINAL:write("> ", {
+        x=0,
+        xPadding=3,
+        textColor=colors.black,
+        bgColor=colors.lightBlue
+    })
+
+    if error then
+        local cursorX, cursorY = TERMINAL.monitor.getCursorPos()
+        term.setTextColor(colors.white)
+        TERMINAL:write(error, {
+            x=0,
+            y=cursorY + 2,
+            xPadding=3,
+            textColor=colors.red,
+            bgColor=colors.lightBlue
+        })
+        TERMINAL.monitor.setCursorPos(cursorX, cursorY)
+    end
+end
+
+local function promptForConfigOption(option)
+    local value = nil
+    local error = nil
+
+    while true do
+        printConfigOption(option, error)
+
+        error = nil
+
+        local readValue = read(nil, nil, nil, value)
+
+        if #readValue == 0 and option.default ~= nil then
+            value = tostring(option.default)
+        else
+            value = readValue
+        end
+
+        if #value == 0 then
+            if option.required then
+                error = "This option is required"
+                goto continue
+            else
+                value = nil
+                break
+            end
+        end
+
+        if option.type == "number" then
+            local casted = tonumber(value) 
+            if casted == nil then
+                error = "This option must be a number"
+                goto continue
+            end
+            value = casted
+        elseif option.type == "boolean" then
+            if value ~= "true" and value ~= "false" then
+                error = "This option must be a boolean (true or false)"
+                goto continue
+            end
+            value = value == "true"
+        end
+
+        if not error then break end
+
+        ::continue::
+    end
+
+    return value
+end
+
+local function startConfiguration(programList)
+    local selectedProgram = programList[SELECTED_PROGRAM_INDEX]
+
+    term.clear()
+
+    local configValues = {}
+
+    for _, option in ipairs(selectedProgram.config) do
+        configValues[option.name] = promptForConfigOption(option)
+    end
+
+    local config = {
+        name=selectedProgram.name,
+        label=selectedProgram.label,
+        description=selectedProgram.description,
+        configValues=configValues
+    }
+
+    local configFile = fs.open(DIR.."/runner/config.json", "w")
+    configFile.write(textutils.serialiseJSON(config))
+    configFile.close()
+    
+    return config
+end
+
+local function runSetup()
+    checkRepoReachable()
 
     local programList = getProgramList()
 
-    printProgramList(programList)
+    startProgramSelection(programList)
 
+    return startConfiguration(programList)
+end
+
+local function getConfigValues()
+    local configFile = fs.open(DIR.."/runner/config.json", "r")
+    if not configFile then return end
+    local contents = textutils.unserialiseJSON(configFile.readAll())
+    configFile.close()
+
+    return contents
+end
+
+local function getIsProgramDownloaded(config)
+    return fs.exists(DIR.."/programs/"..config.name..".lua")
+end
+
+local function getFileDeps(contents)
+    local deps = {}
+
+    for path in contents:gmatch("require%(.(.-).%)") do
+        local fullPath = path..".lua"
+        if utils.tableHasValue(deps, fullPath) then goto continue end
+
+        table.insert(deps, fullPath)
+
+        downloadFileAndDeps(fullPath)
+
+        ::continue::
+    end
+end
+
+local function polyfilDir(contents)
+    return contents:gsub("require%((.)", "require(%1"..DIR)
+end
+
+function downloadFileAndDeps(path)
+    print("  - Downloading "..path)
+
+    local programContents = getFileFromRepo(path)
+
+    getFileDeps(programContents)
+
+    programContents = polyfilDir(programContents)
+    local file = fs.open(DIR..path, "w")
+    file.write(programContents)
+    file.close()
+end
+
+local function downloadFiles(config)
+    local programPath = "/programs/"..config.name..".lua"
+
+    print("- Downloading program and deps for")
+
+    downloadFileAndDeps(programPath)
+end
+
+local QUEUED_COMMAND = nil
+
+local function awaitCommand()
+    local ctrlHeld = false
+
+    print("\n- Available commands:")
+    print("  | Ctrl + S - Stop program")
+    print("  | Ctrl + R - Restart program")
+    print("")
+    print("  | Ctrl + U - Update program")
+    print("  | Ctrl + C - Reconfigure program")
+    print("  | Ctrl + D - Full system reset")
+
+    while true do
+        local event, key = os.pullEvent()
+
+        if event == "key" then
+            if key == keys.leftCtrl then
+                ctrlHeld = true
+            end
+        elseif event == "key_up" then
+            if key == keys.leftCtrl then
+                ctrlHeld = false
+            end
+            
+            if ctrlHeld then
+                if key == keys.s then
+                    QUEUED_COMMAND = "STOP"
+                    return
+                elseif key == keys.r then
+                    QUEUED_COMMAND = "RESTART"
+                    return
+                elseif key == keys.u then
+                    QUEUED_COMMAND = "UPDATE"
+                    return
+                elseif key == keys.c then
+                    QUEUED_COMMAND = "CONFIGURE"
+                    return
+                elseif key == keys.d then
+                    QUEUED_COMMAND = "RESET"
+                    return
+                end
+            end
+        end
+    end
+end
+
+local function runProgram(config)
+    local programPath = DIR.."/programs/"..config.name..".lua"
+
+    print("  - Running program: "..programPath)
+
+    shell.execute(programPath, textutils.serialiseJSON(config.configValues))
+end
+
+local function runWrapper(config)
+    print("- Running wrapper")
+
+    local function run()
+        runProgram(config)
+    end
+
+    parallel.waitForAny(run, awaitCommand)
+end
+
+local function downloadAndRun(config)
+    TERMINAL.monitor.setBackgroundColor(colors.black)
+    TERMINAL.monitor.setTextColor(colors.white)
+    TERMINAL.monitor.clear()
+    TERMINAL.monitor.setCursorPos(1, 1)
+
+    if not getIsProgramDownloaded(config) then
+        checkRepoReachable()
+
+        downloadFiles(config)
+    end
+
+    runWrapper(config)
 end
 
 local function start()
+    local config = getConfigValues()
 
+    if not config then
+        print("- Not configured, running setup")
+        config = runSetup()
+    end
+
+    while true do
+        downloadAndRun(config)
+
+        if QUEUED_COMMAND then
+            print("- Command Recieved: "..QUEUED_COMMAND)
+
+            if QUEUED_COMMAND == "STOP" then
+                QUEUED_COMMAND = nil
+
+                print("- Safely exiting")
+
+                break
+            elseif QUEUED_COMMAND == "RESTART" then
+                QUEUED_COMMAND = nil
+
+                print("- Restarting in 1 second...")
+                os.sleep(1)
+            elseif QUEUED_COMMAND == "UPDATE" then
+                QUEUED_COMMAND = nil
+
+                print("- Performing update")
+                
+                checkRepoReachable()
+
+                downloadFiles(config)
+            elseif QUEUED_COMMAND == "CONFIGURE" then
+                QUEUED_COMMAND = nil
+
+                print("- Starting configuration")
+
+                local programList = getProgramList()
+
+                for i, program in pairs(programList) do
+                    if program.name == config.name then
+                        SELECTED_PROGRAM_INDEX = i
+                    end
+                end
+
+                if SELECTED_PROGRAM_INDEX == nil then
+                    error("Couldn't find program metadata for reconfiguration")
+                end
+
+                config = startConfiguration(programList)
+            elseif QUEUED_COMMAND == "RESET" then
+                QUEUED_COMMAND = nil
+
+                print("- Performing full system reset")
+
+                config = runSetup()
+            end
+        else
+            print("! Program exited, restarting in 5 seconds...")
+            os.sleep(5)
+        end
+    end
 end
 
 start()
