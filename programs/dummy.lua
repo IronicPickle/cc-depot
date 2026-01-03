1f522db0555bc9b1beeda6d25ee74d109105491d
IronicPickle <nathanrath@live.co.uk>
2026-01-03 23:21:56 +0000

    added sleep to dummy program

--- programs/dummy.lua
+++ programs/dummy.lua
@@ -2,5 +2,9 @@ local utils = require("/lib/utils")
 local Monitor = require("/lib/peripherals/Monitor")
 
 local function start()
+    while true do
+        os.sleep(1)
+    end
+
     print("DUMMY TEST")
 end
