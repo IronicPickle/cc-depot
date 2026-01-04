5a83a773b54b7c6d4de99159e05bad8137ddb745
IronicPickle <nathanrath@live.co.uk>
2026-01-04 02:06:57 +0000

    added initial code to door program

--- programs/door.lua
+++ programs/door.lua
@@ -1,5 +1,21 @@
+local Monitor = require("/lib/peripherals/Monitor")
+local Window = require("/lib/peripherals/Window")
 
 
-local function start()
-    print("TEST")
+if not peripheral.isPresent("monitor") then error("An attached monitor is required") end
+if not peripheral.isPresent("modem") then error("An attached modem is required") end
+
+local MONITOR = Monitor:new(peripheral.find("monitor"))
+
+function start()
+    MONITOR.output.clear()
+    MONITOR:write("Test", {
+        x=1,
+        y=2,
+        textColor=colors.white
+    })
+
 end
+
+
+start()
