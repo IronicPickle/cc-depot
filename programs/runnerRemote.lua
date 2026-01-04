0b0c2f61e8b3e7f71b417a85ba8c8ba4964f8145
IronicPickle <nathanrath@live.co.uk>
2026-01-04 15:57:36 +0000

    begun runner remote implementation

--- programs/runnerRemote.lua
+++ programs/runnerRemote.lua
@@ -1,9 +1,93 @@
+-- Deps
+local Monitor = require("/lib/peripherals/Monitor")
+local Window = require("/lib/Window")
+
+-- Peripherals
+local TERMINAL = Monitor:new(term.current())
+local MODEM = peripheral.find("modem")
+
+-- Windows
+local HEADER_HEIGHT = 4
+local HEADER = Window:new(TERMINAL.output, {
+    x=1,
+    y=1,
+    height=HEADER_HEIGHT
+})
+local BODY = Window:new(TERMINAL.output, {
+    x=1,
+    y=HEADER_HEIGHT + 1,
+    height=TERMINAL.output.height - HEADER_HEIGHT
+})
+
+-- Globals
+local RUNNER_UPDATE_CHANNEL = 59941
+local QUEUED_COMMAND = nil
+
+local function drawHeader()
+    HEADER:fillBackground({
+        bgColor=colors.red
+    })
+
+end
+
+local function drawBody()
+    BODY:fillBackground({
+        bgColor=colors.lightGray
+    })
+end
+
+local function awaitCommand()
+    local ctrlHeld = false
+
+    -- print("\n- Available commands:")
+    -- print("  | Ctrl + S - Stop program")
+    -- print("  | Ctrl + R - Restart program")
+    -- print("")
+    -- print("  | Ctrl + U - Update program")
+    -- print("  | Ctrl + C - Reconfigure program")
+    -- print("  | Ctrl + D - Full system reset")
 
 
+
+    while true do
+        local event, p1, _p2, _p3, p4 = os.pullEvent()
+
+        if event == "key_up" then
+            local key = p1
+
+            if key == keys.s then
+                QUEUED_COMMAND = "STOP"
+                return
+            elseif key == keys.r then
+                QUEUED_COMMAND = "RESTART"
+                return
+            elseif key == keys.u then
+                QUEUED_COMMAND = "UPDATE"
+                return
+            elseif key == keys.c then
+                QUEUED_COMMAND = "CONFIGURE"
+                return
+            elseif key == keys.d then
+                QUEUED_COMMAND = "RESET"
+                return
+            end
+        end
+
+        ::continue::
+    end
+end
 
 local function start()
+    -- Allow for runner commands to print
+    os.sleep(1)
+
+    TERMINAL.output.clear()
+
+    drawHeader()
+    drawBody()
+
     while true do
-        os.sleep(5)
+        awaitCommand()
     end
 end
 
